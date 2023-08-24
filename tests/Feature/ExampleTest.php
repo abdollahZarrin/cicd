b65ec328465f0db14b67b45b5d831e90d2e5302d
Abdollah Zarrin <abdollahzarrin@yahoo.com>
2023-08-25 01:40:19 +0330

    add mysql db

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,22 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function test_the_application_returns_a_successful_response_when_user_is_login(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/');
 
         $response->assertStatus(200);
     }
